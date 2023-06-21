450dc7b53cbe7f1da0a791bcd979d98801b4b90e
Mate <mateorc099@gmail.com>
2023-06-21 15:55:50 -0300

    Changing URL to fetch tasks JSON

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -2,17 +2,14 @@ import Navbar from "../Navbar";
 import { Heading, Container } from "@chakra-ui/react";
 import TaskContainer from "../TaskContainer";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
 
 const Home = () => {
   const [tasks, setTasks] = useState([]);
 
-  let { category } = useParams();
-
-  console.log("Category: ", category);
-
   const getTasks = async () => {
-    const response = await fetch("http://localhost:5173/tasks.json");
+    const response = await fetch(
+      "https://pat-4-database.vercel.app/tasks.json"
+    );
     const data = await response.json();
     setTasks(data.tareas);
     console.log(data.tareas);
