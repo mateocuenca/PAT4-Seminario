da978fe83561bb11eb54bf59ccc817a8c23a1695
Mate <mateorc099@gmail.com>
2023-06-21 00:05:21 -0300

    Addinng padding into Task and TaskContainer components

---
+++ src/components/Task/index.jsx
@@ -0,0 +1,162 @@
+import React, { useState } from "react";
+import {
+  Button,
+  Checkbox,
+  HStack,
+  Text,
+  useDisclosure,
+} from "@chakra-ui/react";
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+} from "@chakra-ui/react";
+import CalendarTodayIcon from "@mui/icons-material/CalendarToday";
+import PersonIcon from "@mui/icons-material/Person";
+
+const Task = (props) => {
+  const [checked, setChecked] = useState(false);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const toggleCheck = () => setChecked(!checked);
+  const dayNames = [
+    "Domingo",
+    "Lunes",
+    "Martes",
+    "Miércoles",
+    "Jueves",
+    "Viernes",
+    "Sábado",
+  ];
+
+  const isToday = function (date) {
+    const today = new Date();
+
+    if (today.toDateString() === date.toDateString()) {
+      return true;
+    }
+
+    return false;
+  };
+
+  const isYesterday = function (date) {
+    const yesterday = new Date();
+    yesterday.setDate(yesterday.getDate() - 1);
+
+    console.log(date);
+
+    if (yesterday.toDateString() === date.toDateString()) {
+      return true;
+    }
+
+    return false;
+  };
+
+  const isTomorrow = function (date) {
+    const tomorrow = new Date();
+    tomorrow.setDate(tomorrow.getDate() + 1);
+
+    console.log(tomorrow);
+
+    if (tomorrow.toDateString() === date.toDateString()) {
+      return true;
+    }
+
+    return false;
+  };
+
+  return (
+    <>
+      <HStack gap={4}>
+        <Checkbox
+          colorScheme="orange"
+          size="lg"
+          spacing="1rem"
+          borderColor="orange.500"
+          onChange={toggleCheck}
+        ></Checkbox>
+        <Text
+          color="orange.600"
+          fontSize="lg"
+          textDecoration={checked ? "line-through" : "none"}
+          onClick={onOpen}
+          cursor="pointer"
+        >
+          {props.taskTitle}
+        </Text>
+        <Modal isOpen={isOpen} onClose={onClose}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Modificar Tarea</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody></ModalBody>
+
+            <ModalFooter>
+              <Button colorScheme="orange" mr={3} onClick={onClose}>
+                Close
+              </Button>
+              <Button colorScheme="orange" variant="ghost">
+                Secondary Action
+              </Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+      </HStack>
+      <HStack justifyContent="start" py={2} px={10}>
+        <Text
+          fontSize="md"
+          color={
+            isToday(props.taskDate)
+              ? "green"
+              : isYesterday(props.taskDate)
+              ? "red"
+              : isTomorrow(props.taskDate)
+              ? "blue"
+              : "purple"
+          }
+          fontWeight="bold"
+          display="flex"
+          alignItems="center"
+          gap={2}
+          textDecoration={checked ? "line-through" : "none"}
+          onClick={onOpen}
+          cursor="pointer"
+        >
+          <CalendarTodayIcon sx={{}} />{" "}
+          {isToday(props.taskDate)
+            ? "Hoy"
+            : isYesterday(props.taskDate)
+            ? "Ayer"
+            : isTomorrow(props.taskDate)
+            ? "Mañana"
+            : `${
+                dayNames[props.taskDate.getDay()]
+              } ${props.taskDate.getDate()}/${
+                String(props.taskDate.getMonth()).length > 1
+                  ? props.taskDate.getMonth() + 1
+                  : "0" + (props.taskDate.getMonth() + 1)
+              }`}
+        </Text>
+        <Text
+          fontSize="md"
+          color="orange.500"
+          fontWeight="bold"
+          display="flex"
+          alignItems="center"
+          gap={2}
+          textDecoration={checked ? "line-through" : "none"}
+          onClick={onOpen}
+          cursor="pointer"
+        >
+          <PersonIcon />
+          {props.taskOwner}
+        </Text>
+      </HStack>
+    </>
+  );
+};
+
+export default Task;
